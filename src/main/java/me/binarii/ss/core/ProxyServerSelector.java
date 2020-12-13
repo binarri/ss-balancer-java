bf768a91736216f4c720f6ae392bbb54f497c70e
binarii <karl2009@foxmail.com>
2020-12-13 21:40:00 +0800

    optimize, bug fix

--- src/main/java/me/binarii/ss/core/ProxyServerSelector.java
+++ src/main/java/me/binarii/ss/core/ProxyServerSelector.java
@@ -2,52 +2,29 @@ package me.binarii.ss.core;
 
 import me.binarii.ss.model.ProxyServer;
 
-import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
 public class ProxyServerSelector {
 
-	private static final int DEFAULT_THRESHOLD = 1000;
+    private final List<ProxyServer> proxyServers;
 
-	private volatile int threshold = DEFAULT_THRESHOLD;
+    public ProxyServerSelector(List<ProxyServer> proxyServers) {
+        this.proxyServers = proxyServers;
+    }
 
-	private List<ProxyServer> proxyServers;
+    public List<ProxyServer> selectProxyServers() {
+        synchronized (ProxyServer.LOCK) {
+            Long minValue = proxyServers.stream()
+                    .min(ProxyServer::compareTo)
+                    .map(ProxyServer::getLatency)
+                    .orElse(0L);
 
-	private List<ProxyServer> defaultProxy;
-
-	public ProxyServerSelector(List<ProxyServer> proxyServers) {
-		this.proxyServers = proxyServers;
-		this.defaultProxy = Collections.singletonList(proxyServers.get(0));
-	}
-
-	public List<ProxyServer> selectProxyServers() {
-		synchronized (ProxyServer.LOCK) {
-			Long minValue = proxyServers.stream()
-					.min(ProxyServer::compareTo)
-					.map(ProxyServer::getLatency)
-					.orElse(0L);
-
-			List<ProxyServer> hosts = proxyServers.stream()
-					.filter(proxy -> proxy.getLatency() < threshold)
-					.collect(toList());
-
-			if (hosts.isEmpty()) {
-				hosts = proxyServers.stream()
-						.min(ProxyServer::compareTo)
-						.map(Collections::singletonList)
-						.orElse(defaultProxy);
-			}
-
-			if (hosts.size() > 1) {
-				hosts = proxyServers.stream()
-						.filter(proxy -> proxy.getLatency() - minValue <= 200)
-						.collect(toList());
-			}
-
-			return hosts;
-		}
-	}
+            return proxyServers.stream()
+                    .filter(proxy -> proxy.getLatency() - minValue <= 200)
+                    .collect(toList());
+        }
+    }
 
 }
